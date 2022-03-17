daf3d8354ee06661c1f8f843b2807ba832032c31
jerrybassat7 <jerry.bassat@upr.edu>
2022-03-17 14:34:58 -0400

    adding deploy scripts

--- scripts/deploy-seed.js
+++ scripts/deploy-seed.js
@@ -2,70 +2,39 @@ require('dotenv').config()
 const hre = require("hardhat");
 const ethers = hre.ethers
 
-// TODO must be rewritten
-
 const DeployUtils = require('./lib/DeployUtils')
 let deployUtils
 
 async function main() {
   deployUtils = new DeployUtils(ethers)
   const chainId = await deployUtils.currentChainId()
-  let [, localTokenOwner, localSuperAdmin] = await ethers.getSigners();
-  // let tx;
+  let [, , localSuperAdmin] = await ethers.getSigners();
 
-  const tokenOwner = chainId === 1337
-      ? localTokenOwner.address
-      : process.env.TOKEN_OWNER
+  const network = chainId === 56 ? 'BSC'
+      : chainId === 97 ? 'BSCTestnet'
+          : 'localhost'
 
   const superAdmin = chainId === 1337
       ? localSuperAdmin.address
       : process.env.SUPER_ADMIN
 
-  const maxTotalSupply = process.env.MAX_TOTAL_SUPPLY || 10000000000
-
-  console.log('Deploying SyndicateERC20...')
-  const SYN = await ethers.getContractFactory("SyndicateERC20")
-  const syn = await SYN.deploy(tokenOwner, maxTotalSupply, superAdmin)
-  await syn.deployed()
-  console.log('SyndicateERC20 deployed at', syn.address)
-
-  let notReallyDeployedYet = true
-  let features
-
-  // if the network is congested the following can fail
-  while (notReallyDeployedYet) {
-    try {
-      features =
-          (await syn.FEATURE_TRANSFERS_ON_BEHALF()) +
-          (await syn.FEATURE_TRANSFERS()) +
-          (await syn.FEATURE_UNSAFE_TRANSFERS()) +
-          (await syn.FEATURE_DELEGATIONS()) +
-          (await syn.FEATURE_DELEGATIONS_ON_BEHALF())
-      notReallyDeployedYet = false
-    } catch (e) {
-      await deployUtils.sleep(1000)
-    }
-  }
-  await (await syn.updateFeatures(features)).wait()
-
-  const network = chainId === 1 ? 'ethereum'
-      : chainId == 42 ? 'kovan'
-          : 'localhost'
+  console.log('Deploying SEED...')
+  const SeedToken = await ethers.getContractFactory("SeedToken")
+  const seed = await upgrades.deployProxy(SeedToken, []);
+  await seed.deployed()
 
   console.log(`
-To verify SyndicateERC20 source code:
+To verify SeedToken source code:
     
   npx hardhat verify --show-stack-traces \\
       --network ${network} \\
-      ${syn.address} \\
-      ${tokenOwner} \\
-      ${maxTotalSupply} \\
-      ${superAdmin} 
+      ${seed.address}  \\
+      ${superAdmin}
       
 `)
 
-
-  await deployUtils.saveDeployed(chainId, ['SyndicateERC20'], [syn.address])
+  console.log('SeedToken deployed at', seed.address)
+  await deployUtils.saveDeployed(chainId, ['SeedToken'], [seed.address])
 }
 
 main()
--- scripts/deploy-seedfactory.js
+++ scripts/deploy-seedfactory.js
@@ -20,98 +20,33 @@ async function main() {
 
   const [owner] = await ethers.getSigners()
 
-  const synPerBlock = process.env.SYN_PER_BLOCK || '360000000000000000000'
-  const blockPerUpdate = process.env.BLOCK_PER_UPDATE || 91252
-  const threeYearsBlocks = process.env.THREE_YEARS_BLOCKS || 7120725
-  const weight = process.env.WEIGHT || 200
-  const delay = process.env.DELAY || 6460 // 24 hours
+ 
+  const seed = deployed[chainId].SeedToken
 
-  const synAddress = deployed[chainId].SyndicateERC20
-  const ssynAddress = deployed[chainId].SyntheticSyndicateERC20
-  console.log('Deploying SyndicatePoolFactory')
-  const PoolFactory = await ethers.getContractFactory("SyndicatePoolFactory")
-  const blockNumberFactoryConstructor = (await ethers.provider.getBlockNumber())
-      // + 1
-      //(chainId === 1 ? delay : chainId === 42 ? 40 : 1)
+  console.log('Deploying SeedFactory')
+  const SeedFactory = await ethers.getContractFactory("SeedFactoryMock")
 
-  const poolFactory = await PoolFactory.deploy(
-      synAddress,
-      ssynAddress,
-      ethers.BigNumber.from(synPerBlock),
-      ethers.BigNumber.from(blockPerUpdate),
-      blockNumberFactoryConstructor,
-      blockNumberFactoryConstructor + parseInt(threeYearsBlocks)
-  );
-  await poolFactory.deployed()
-  console.log('SyndicatePoolFactory deployed at', poolFactory.address)
+  const seedFactory = await upgrades.deployProxy(SeedFactory, [seed.address]);
+  await seedFactory.deployed()
 
-  const network = chainId === 1 ? 'ethereum'
-      : chainId === 42 ? 'kovan'
+
+  console.log('SeedFactoryMock deployed at', seedFactory.address)
+
+  const network = chainId === 56 ? 'BSC'
+      : chainId === 97 ? 'BSCTestnet'
           : 'localhost'
 
   console.log(`
-To verify SyndicatePoolFactory source code:
-    
+To verify SeedFactoryMock source code:
+
   npx hardhat verify --show-stack-traces \\
       --network ${network} \\
       ${poolFactory.address} \\
-      ${synAddress} \\
-      ${ssynAddress} \\
-      ${ethers.BigNumber.from(synPerBlock).toString()} \\
-      ${ethers.BigNumber.from(blockPerUpdate).toString()} \\
-      ${blockNumberFactoryConstructor} \\
-      ${blockNumberFactoryConstructor + parseInt(threeYearsBlocks)}
-      
+      ${seed.address} \\
 `)
 
-  const blockNumberPoolCreation = await ethers.provider.getBlockNumber()
-
-  console.log('Creating SyndicateCorePool')
-  const tx = await poolFactory.connect(owner).createPool(synAddress, blockNumberPoolCreation, weight)
-  await tx.wait()
-
-  const synPoolAddress = await poolFactory.getPoolAddress(synAddress)
-  const corePool = await deployUtils.getContract('SyndicateCorePool', 'pools', synPoolAddress, chainId)
-  console.log('SyndicateCorePool deployed at', corePool.address)
-
-  console.log(`
-To verify SyndicateCorePool source code:
-    
-  npx hardhat verify --show-stack-traces \\
-      --network ${network} \\
-      ${corePool.address} \\
-      ${synAddress} \\
-      ${ssynAddress} \\
-      ${poolFactory.address} \\
-      ${synAddress} \\
-      ${blockNumberPoolCreation} \\
-      ${weight}
-      
-`)
-
-  // await corePool.connect(owner).setQuickRewardRate(ethers.BigNumber.from(process.env.QUICK_REWARDS))
-  // console.log('Quick reward set')
-
-  const SYN = await ethers.getContractFactory("SyndicateERC20")
-  const syn = await SYN.attach(deployed[chainId].SyndicateERC20)
-
-  const SSYN = await ethers.getContractFactory("SyntheticSyndicateERC20")
-  const ssyn = await SSYN.attach(deployed[chainId].SyntheticSyndicateERC20)
-
-  // for safety we will give this role to the pool only before the 16 weeks ends
-  // await syn.connect(owner).updateRole(poolFactory.address, await syn.ROLE_TOKEN_CREATOR());
-
-  await ssyn.connect(owner).updateRole(corePool.address, await ssyn.ROLE_TOKEN_CREATOR());
-  console.log('Pool authorized to manage sSYN')
-
-  await deployUtils.saveDeployed(chainId,
-      ['SyndicatePoolFactory', 'SyndicateCorePool'],
-      [poolFactory.address, corePool.address],
-      {
-        blockNumberFactoryConstructor,
-        blockNumberPoolCreation
-      }
-  )
+console.log('SeedFactoryMock deployed at', seedFactory.address)
+await deployUtils.saveDeployed(chainId, ['SeedFactoryMock'], [seedFactory.address])
 
 }
 
