bbf75415a9f1486c1b3bcea52d6bafc74e04771c
Francesco Sullo <sullof@gmail.com>
2022-04-15 18:00:34 -0500

    Minor change

--- scripts/lib/PayloadUtils.js
+++ scripts/lib/PayloadUtils.js
@@ -29,35 +29,6 @@ const PayloadUtils = {
       .add(await BNMulBy(lockupTime, 100))
       .add(await BNMulBy(tokenAmountOrID, 1, 5));
   },
-
-  async validateInput(tokenType, lockupTime, tokenAmountOrID) {
-    if (tokenType > 99) return "PayloadUtils: invalid token type";
-    if (tokenType === SYNR_PASS_STAKE_FOR_BOOST || tokenType === SYNR_PASS_STAKE_FOR_SEEDS) {
-      if (tokenAmountOrID > 888) return "PayloadUtils: Not a Mobland SYNR Pass token ID";
-    } else if (tokenType === BLUEPRINT_STAKE_FOR_BOOST) {
-      if (tokenAmountOrID > 8000) return "PayloadUtils: Not a Blueprint token ID";
-    } else if (tokenAmountOrID > 1e28 - 1) return "PayloadUtils: tokenAmountOrID out of range";
-
-    if (lockupTime > 1e3 - 1) return "PayloadUtils: lockedTime out of range";
-  },
-  //   // can be called by tests and web2 app
-  //   function serializeInput(
-  //       uint256 tokenType, // 2 digit
-  //   uint256 lockupTime, // 3 digits
-  //   uint256 tokenAmountOrID
-  // ) external pure override returns (uint256 payload) {
-  //   validateInput(tokenType, lockupTime, tokenAmountOrID);
-  //   payload = tokenType.add(lockupTime.mul(100)).add(tokenAmountOrID.mul(1e5));
-  // }
-  //
-  // function validateInput(
-  //     uint256 tokenType,
-  //     uint256 lockupTime,
-  //     uint256 tokenAmountOrID
-  // ) public pure override returns (bool) {
-
-  //   return true;
-  // }
 };
 
 module.exports = PayloadUtils;
