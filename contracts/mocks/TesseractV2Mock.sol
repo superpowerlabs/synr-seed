a096d8d9f0d1bd245a4b52f4d755fac7b31412b3
Francesco Sullo <sullof@gmail.com>
2022-05-11 10:14:01 -0700

    Adding new bridge type to TesseractV2Mock

--- contracts/mocks/TesseractV2Mock.sol
+++ contracts/mocks/TesseractV2Mock.sol
@@ -4,6 +4,7 @@ pragma solidity 0.8.11;
 import "../Tesseract.sol";
 
 import "hardhat/console.sol";
+import "./IWormholeBridgeV2.sol";
 
 contract TesseractV2Mock is Tesseract {
   function version() external pure override returns (uint256) {
@@ -21,7 +22,12 @@ contract TesseractV2Mock is Tesseract {
         IWormholeBridge(bridges[1]).wormholeTransfer(payload, recipientChain, bytes32(uint256(uint160(_msgSender()))), nonce);
     } else if (bridgeType == 2) {
       return
-        IWormholeBridge(bridges[1]).wormholeTransfer(payload, recipientChain, bytes32(uint256(uint160(_msgSender()))), nonce);
+        IWormholeBridgeV2(bridges[1]).wormholeTransferV2(
+          payload,
+          recipientChain,
+          bytes32(uint256(uint160(_msgSender()))),
+          nonce
+        );
     } else {
       revert("Tesseract: unsupported bridge");
     }
