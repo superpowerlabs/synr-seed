2d7007069a1ce150a7919413b54872f9accdd106
Francesco Sullo <sullof@gmail.com>
2022-06-05 20:12:25 -0700

    Minor change 3

--- scripts/parameters.js
+++ scripts/parameters.js
@@ -18,7 +18,7 @@ module.exports = {
   sPBoostFactor: 20000,
   sPBoostLimit: 200000,
   bPSynrEquivalent: 3000,
-  bPBoostFactor: 12000,
+  bPBoostFactor: 13220,
   bPBoostLimit: 6000,
 
   // initial values
